203e04eb83d30213edd0f149b1c0c070af6786d5
Ishika Verma <ishikaverma2317@gmail.com>
2025-09-21 22:03:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+function setBackground() {
+  if (window.innerWidth > window.innerHeight) {
+    // Landscape Mode
+    document.body.style.backgroundImage =
+      "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
+  } else {
+    // Portrait Mode
+    document.body.style.backgroundImage =
+      "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
+  }
+}
+
+// Run on page load
+setBackground();
+
+// Update on resize
+window.addEventListener("resize", setBackground);
